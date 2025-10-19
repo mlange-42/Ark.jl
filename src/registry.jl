bb54bf406557b1a483a6e7ae50f8fc9e7cff2109
mlange-42 <martin_lange_@gmx.net>
2025-10-19 11:22:30 +0200

    implement component ID registration

--- src/registry.jl
+++ src/registry.jl
@@ -1,8 +1,23 @@
 
-struct ComponentRegistry
+
+mutable struct ComponentRegistry
+    counter::UInt8
     components::Dict{DataType, UInt8}
 end
 
 function ComponentRegistry()
-    ComponentRegistry(Dict{DataType, UInt8}())
+    ComponentRegistry(0x00, Dict{DataType, UInt8}())
+end
+
+function component_id!(registry::ComponentRegistry, ::Type{T}) where T
+    if haskey(registry.components, T)
+        return registry.components[T]
+    elseif registry.counter == typemax(UInt8)
+        error("ComponentRegistry exhausted UInt8 range")
+    else
+        id = registry.counter
+        registry.components[T] = id
+        registry.counter += 0x01
+        return id
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Ark
 using Test
 
 include("../src/Ark.jl")
+include("../src/registry.jl")
 
 @testset "World creation" begin
     world = Ark.World()
@@ -9,4 +10,22 @@ include("../src/Ark.jl")
     @test isa(world.registry, Ark.ComponentRegistry)
     @test world.storages == Vector{Any}()
     @test world.archetypes == Vector{Ark.Archetype}()
+end
+
+@testset "ComponentRegistry Tests" begin
+    registry = ComponentRegistry()
+
+    # Register new types
+    id_int = component_id!(registry, Int)
+    id_float = component_id!(registry, Float64)
+    id_string = component_id!(registry, String)
+
+    # Check that IDs are UInt8 and unique
+    @test isa(id_int, UInt8)
+    @test isa(id_float, UInt8)
+    @test isa(id_string, UInt8)
+    @test id_int != id_float != id_string
+
+    # Check repeated registration returns same ID
+    @test component_id!(registry, Int) == id_int
 end
