50f46ee3617b83a82463f4855b55b8821e4681df
mlange-42 <martin_lange_@gmx.net>
2025-10-19 21:40:20 +0200

    use function overloads

--- src/map.jl
+++ src/map.jl
@@ -19,14 +19,14 @@ function Map2{A,B}(world::World) where {A,B}
     )
 end
 
-function get2(map::Map2{A,B}, entity::Entity)::Tuple{A,B} where {A,B}
+function get_components(map::Map2{A,B}, entity::Entity)::Tuple{A,B} where {A,B}
     index = map._world._entities[entity._id]
     a = map._storage_a.data[index.archetype][index.row]
     b = map._storage_b.data[index.archetype][index.row]
     return a, b
 end
 
-function new_entity2(map::Map2{A,B}, a::A, b::B) where {A,B}
+function new_entity!(map::Map2{A,B}, a::A, b::B) where {A,B}
     archetype = _find_or_create_archetype!(map._world, map._ids...)
     entity, index = _create_entity!(map._world, archetype)
     map._storage_a.data[archetype][index] = a
--- test/test_map.jl
+++ test/test_map.jl
@@ -3,14 +3,13 @@ using Test
 
 using .TestTypes: Position, Velocity
 
-
-@testset "Map creation" begin
+@testset "Map operations" begin
     world = World()
     m = Map2{Position,Velocity}(world)
 
-    entity = new_entity2(m, Position(1, 2), Velocity(3, 4))
+    entity = new_entity!(m, Position(1, 2), Velocity(3, 4))
 
-    pos, vel = get2(m, entity)
+    pos, vel = get_components(m, entity)
     @test pos == Position(1, 2)
     @test vel == Velocity(3, 4)
 end
