aa9289af473ba296e1cbf70157b38e0f0093e345
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-21 00:39:43 +0200

    Update map.jl

--- src/map.jl
+++ src/map.jl
@@ -1,9 +1,15 @@
 
+@generated function get_mapped_components(map::Map{CS}, index::Index) where {CS <: Tuple}
+    N = length(CS.parameters)
+    expressions = [:(map._storage[$i].data[index.archetype][index.row]) for i in 1:N]
+    tuple_expr = Expr(:tuple, expressions...)
+    return $tuple_expr
+end
 
 struct Map{CS<:Tuple, N}
     _world::World
     _ids::NTuple{N, UInt8}
-    _storace::CS
+    _storage::CS
 end
 
 function Map(world::World, comps::Tuple)
@@ -11,6 +17,36 @@ function Map(world::World, comps::Tuple)
     return Map(world, ids, _get_storage.(world, ids, comps))
 end
 
+function get_components(map::Map, entity::Entity)
+    return map[entity]
+end
+
+function remove_components!(map::Map, entity::Entity)
+    if !is_alive(map._world, entity)
+        error("can't remove components from a dead entity")
+    end
+    archetype = _find_or_create_archetype!(map._world, entity, (), map._ids)
+    _move_entity!(map._world, entity, archetype)
+end
+
+function set_components!(map::Map, entity::Entity, values)
+    map[entity] = values
+end
+
+#################################
+
+@inline function Base.getindex(map::Map, entity::Entity)
+    if !is_alive(map._world, entity)
+        error("can't get components of a dead entity")
+    end
+    # TODO: currently raises MethodError of components are missing.
+    # Should we pay the cost for a more informative error,
+    # or for returning nothing?
+    index = map._world._entities[entity._id]
+    return a get_mapped_components(map, index)
+end
+
+# todo: generation
 function new_entity!(map::Map, comps::Tuple)
     archetype =
         _find_or_create_archetype!(map._world, map._world._archetypes[1].node, map._ids, ())
@@ -19,38 +55,8 @@ function new_entity!(map::Map, comps::Tuple)
     return entity
 end
 
-function get_components(map::Map, entity::Entity)
-    return map[entity]
-end
-
-@inline function Base.getindex(map::Map1{A}, entity::Entity)::Tuple{A} where {A}
-    if !is_alive(map._world, entity)
-        error("can't get components of a dead entity")
-    end
-    # TODO: currently raises MethodError of components are missing.
-    # Should we pay the cost for a more informative error,
-    # or for returning nothing?
-    index = map._world._entities[entity._id]
-    a = map._storage_a.data[index.archetype][index.row]
-    return a
-end
-
-"""
-    set_components!(map::Map1{ A }, entity::Entity, a::A)
-
-Set 1 components of an [`Entity`](@ref).
-
-Alternatively, use indexing:
-
-```julia
-map[entity] = (Position(0, 0), Velocity(1, 1))
-```
-"""
-function set_components!(map::Map1{A}, entity::Entity, a::A) where {A}
-    map[entity] = (a)
-end
-
-@inline function Base.setindex!(map::Map1{A}, value::Tuple{A}, entity::Entity) where {A}
+# todo: generation
+@inline function Base.setindex!(map::Map, value, entity::Entity)
     if !is_alive(map._world, entity)
         error("can't set components of a dead entity")
     end
@@ -58,12 +64,8 @@ end
     map._storage_a.data[index.archetype][index.row] = value[1]
 end
 
-"""
-    has_components!(map::Map1{ A }, entity::Entity)
-
-Returns whether an [`Entity`](@ref) has the given components.
-"""
-function has_components(map::Map1{A}, entity::Entity) where {A}
+# todo: generation
+function has_components(map::Map, entity::Entity)
     if !is_alive(map._world, entity)
         error("can't check components of a dead entity")
     end
@@ -74,70 +76,12 @@ function has_components(map::Map1{A}, entity::Entity) where {A}
     return true
 end
 
-"""
-    add_components!(map::Map1{ A }, a::A)::Entity
-
-Adds 1 components to an [`Entity`](@ref).
-"""
-function add_components!(map::Map1{A}, entity::Entity, a::A) where {A}
+# todo: generation
+function add_components!(map::Map, entity::Entity, value)
     if !is_alive(map._world, entity)
         error("can't add components to a dead entity")
     end
     archetype = _find_or_create_archetype!(map._world, entity, map._ids, ())
     row = _move_entity!(map._world, entity, archetype)
     map._storage_a.data[archetype][row] = a
-end
-
-"""
-    remove_components!(map::Map1{ A }, entity::Entity)
-
-Removes 1 components from an [`Entity`](@ref).
-"""
-function remove_components!(map::Map1{A}, entity::Entity) where {A}
-    if !is_alive(map._world, entity)
-        error("can't remove components from a dead entity")
-    end
-    archetype = _find_or_create_archetype!(map._world, entity, (), map._ids)
-    _move_entity!(map._world, entity, archetype)
-end
-
-@inline function Base.getindex(map::Map2{A,B}, entity::Entity)::Tuple{A,B} where {A,B}
-    if !is_alive(map._world, entity)
-        error("can't get components of a dead entity")
-    end
-    # TODO: currently raises MethodError of components are missing.
-    # Should we pay the cost for a more informative error,
-    # or for returning nothing?
-    index = map._world._entities[entity._id]
-    a = map._storage_a.data[index.archetype][index.row]
-    b = map._storage_b.data[index.archetype][index.row]
-    return a, b
-end
-
-"""
-    set_components!(map::Map2{ A,B }, entity::Entity, a::A, b::B)
-
-Set 2 components of an [`Entity`](@ref).
-
-Alternatively, use indexing:
-
-```julia
-map[entity] = (Position(0, 0), Velocity(1, 1))
-```
-"""
-function set_components!(map::Map2{A,B}, entity::Entity, a::A, b::B) where {A,B}
-    map[entity] = (a, b)
-end
-
-@inline function Base.setindex!(
-    map::Map2{A,B},
-    value::Tuple{A,B},
-    entity::Entity,
-) where {A,B}
-    if !is_alive(map._world, entity)
-        error("can't set components of a dead entity")
-    end
-    index = map._world._entities[entity._id]
-    map._storage_a.data[index.archetype][index.row] = value[1]
-    map._storage_b.data[index.archetype][index.row] = value[2]
 end
