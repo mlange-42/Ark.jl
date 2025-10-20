4c8200ed51cdc3498cef55d1015c0a876633f427
mlange-42 <martin_lange_@gmx.net>
2025-10-20 14:39:48 +0200

    complete maps template

--- dev/generate_maps.jl
+++ dev/generate_maps.jl
@@ -8,11 +8,16 @@ items = []
 for i = 1:3
     types = [Dict("n" => idx, "upper" => string(Char(c)), "lower" => string(lowercase(Char(c)))) for (idx, c) in enumerate('A':'A'+i-1)]
     joined = join([string(Char(c)) for c in 'A':'A'+i-1], ",")
+    tuple = join(fill("UInt8", i), ",")
+    args = join([string(d["lower"], "::", d["upper"]) for d in types], ", ")
+    values = join([d["lower"] for d in types], ", ")
     item = Dict(
         "N" => i,
         "types" => types,
         "joined" => joined,
-        "tuple" => join(fill("UInt8", i), ","),
+        "tuple" => tuple,
+        "args" => args,
+        "values" => values,
     )
 
     push!(items, item)
