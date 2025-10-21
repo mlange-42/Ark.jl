51d4f548047e547fd07585cd53c4eb46050f93a5
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-21 11:47:54 +0200

    Update run_benchmarks.jl

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -2,6 +2,6 @@ using BenchmarkTools
 using Ark
 
 include("BenchTypes.jl")
-#include("bench_query_posvel.jl")
+include("bench_query_posvel.jl")
 include("bench_map_posvel.jl")
 include("bench_map_get.jl")
