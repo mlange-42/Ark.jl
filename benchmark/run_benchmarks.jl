98689ebcc3da1479768e39141eb86ef8d0a76b1c
mlange-42 <martin_lange_@gmx.net>
2025-10-21 16:01:44 +0200

    add benchmarks for add/remove component

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -21,9 +21,11 @@ seconds = args["seconds"]
 
 include("BenchTypes.jl")
 
+
 include("bench_query_posvel.jl")
 include("bench_map_posvel.jl")
 include("bench_map_get_1.jl")
 include("bench_map_get_5.jl")
 include("bench_new_entity_1.jl")
 include("bench_new_entity_5.jl")
+include("bench_add_remove.jl")
