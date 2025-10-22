9d0fc24bb2da5699cbfdb3e4314e1bd3ea45b5bb
mlange-42 <martin_lange_@gmx.net>
2025-10-23 00:23:07 +0200

    remove unused functions

--- src/storage.jl
+++ src/storage.jl
@@ -3,10 +3,6 @@ struct _ComponentStorage{C}
     data::Vector{Union{Nothing,Column{C}}}  # Outer Vec: one per archetype
 end
 
-function _ComponentStorage{C}() where C
-    _ComponentStorage{C}(Vector{Union{Nothing,Column{C}}}())
-end
-
 function _ComponentStorage{C}(archetypes::Int) where C
     _ComponentStorage{C}(Vector{Union{Nothing,Column{C}}}(nothing, archetypes))
 end
