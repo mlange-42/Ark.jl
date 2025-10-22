4cd2ec145c646a01aa5ee4906ccf7364861b6d94
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-22 03:13:31 +0200

    Update bench_query_posvel.jl

--- benchmark/bench_query_posvel.jl
+++ benchmark/bench_query_posvel.jl
@@ -24,5 +24,5 @@ function benchmark_query_posvel(args, n)
 end
 
 for n in (100, 1_000, 10_000, 100_000)
-    SUITE["benchmark_query_posvel n=$n"] = setup_query_posvel(n) benchmark_query_posvel(_, $n)
+    SUITE["benchmark_query_posvel n=$n"] = @benchmarkable setup_query_posvel($n) benchmark_query_posvel(_, $n)
 end
