7e5802a8e762128b4a4da55696db376c70bf7863
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-22 04:13:35 +0200

    Update run_benchmarks.jl

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,8 +1,10 @@
 
+# run with julia run_benchmarks.jl dev-branch-name
 using Pkg
 
 if !("AirspeedVelocity" in keys(Pkg.project().dependencies))
     Pkg.add("AirspeedVelocity"); Pkg.build("AirspeedVelocity")
 end
 
-~/.julia/bin/benchpkg --add https://github.com/LilithHafner/ChairmarksForAirspeedVelocity.jl --rev dirty,main
+devbranch = ARGS[1]
+run(`$(joinpath(homedir(), ".julia/bin/benchpkg")) --add https://github.com/LilithHafner/ChairmarksForAirspeedVelocity.jl --rev main,$devbranch --bench-on $devbranch`)
