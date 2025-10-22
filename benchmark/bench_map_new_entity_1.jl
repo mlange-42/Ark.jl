dc59697c27d2a08273987d225b932dff054b3fc1
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-23 01:01:37 +0200

    Update bench_map_new_entity_1.jl

--- benchmark/bench_map_new_entity_1.jl
+++ benchmark/bench_map_new_entity_1.jl
@@ -25,5 +25,5 @@ function benchmark_new_entity_1(args, n)
 end
 
 for n in (100, 1_000, 10_000, 100_000)
-    SUITE["benchmark_new_entity_1 n=$n"] = @benchmarkable setup_new_entity_1($n) benchmark_new_entity_1(_, $n)
+    SUITE["benchmark_new_entity_1 n=$n"] = @benchmarkable setup_map_new_entity_1($n) benchmark_new_entity_1(_, $n)
 end
