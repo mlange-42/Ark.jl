2c59bb86f685e7cfe085f3d160b969628df2dc40
mlange-42 <martin_lange_@gmx.net>
2025-10-22 10:53:30 +0200

    benchmark world getter

---
+++ benchmark/bench_world_get_1.jl
@@ -0,0 +1,43 @@
+
+println("-----------------------------------------------")
+println("                Map get 1")
+println("-----------------------------------------------")
+
+function setup_map_get_1(n_entities::Int)
+    world = World(Position, Velocity)
+    map = Map(world, (Position,))
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(map, (Position(i, i * 2),))
+        push!(entities, e)
+    end
+
+    sum = 0.0
+    for e in entities
+        pos, = get_components(world, e, Position)
+        sum += pos.x
+    end
+    sum
+
+    return (entities, world)
+end
+
+function benchmark_map_get_1(n)
+    bench = @benchmarkable begin
+        sum = 0.0
+        for e in entities
+            pos, = get_components(world, e, Position)
+            sum += pos.x
+        end
+        sum
+    end setup = ((entities, world) = setup_map_get_1($n))
+
+    tune!(bench)
+    result = run(bench, seconds=seconds)
+    print_result(result, n)
+end
+
+for n in (100, 1_000, 10_000, 100_000)
+    benchmark_map_get_1(n)
+end
