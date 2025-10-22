0e859a2492759690cc97a4b13125138f4f371f67
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-23 00:15:40 +0200

    Update benchmark script to handle branch arguments

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,9 +1,14 @@
-# run with: julia run_benchmarks.jl dev-branch-name
+# run with julia run_benchmarks.jl [dev-branch-name]
 using Pkg
 
 if !("AirspeedVelocity" in keys(Pkg.project().dependencies))
     Pkg.add("AirspeedVelocity"); Pkg.build("AirspeedVelocity")
 end
 
-devbranch = ARGS[1]
-run(`$(joinpath(homedir(), ".julia/bin/benchpkg")) --add https://github.com/LilithHafner/ChairmarksForAirspeedVelocity.jl --rev main,$devbranch --bench-on $devbranch`)
+if isempty(ARGS)
+    devbranch = run(`git branch --show-current`)
+    run(`$(joinpath(homedir(), ".julia/bin/benchpkg")) --add https://github.com/LilithHafner/ChairmarksForAirspeedVelocity.jl --rev $devbranch --bench-on $devbranch`)
+else
+    devbranch = ARGS[1]
+    run(`$(joinpath(homedir(), ".julia/bin/benchpkg")) --add https://github.com/LilithHafner/ChairmarksForAirspeedVelocity.jl --rev main,$devbranch --bench-on $devbranch`)
+end
