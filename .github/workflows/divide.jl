f2848c08fb4d20f65d469ebf5d5e458da0607358
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-23 03:18:11 +0200

    Update divide.jl

--- .github/workflows/divide.jl
+++ .github/workflows/divide.jl
@@ -1,19 +1,22 @@
+
 using Pkg
 Pkg.add("JSON")
 using JSON
 
 results_dir = ARGS[1]
-json_file = filter(f -> endswith(f, ".json"), readdir(results_dir))[1]
-json_text = read(json_file, String) # I need to find branch somehow
-data = JSON.parse(json_text)
-for x in pairs(data["data"])
-	m = match(r"n=(\d+)", x[1])
-	if m != nothing 
-	    n = parse(Int, m.captures[1])
-	    x[2]["times"] ./= n
-	end
-end
+json_files = filter(f -> endswith(f, ".json"), readdir(results_dir))
 
-open(json_file, "w") do f
-    JSON.print(f, data) 
+for file in json_files
+	json_text = read(joinpath(results_dir, file), String) # I need to find branch somehow
+	data = JSON.parse(json_text)
+	for x in pairs(data["data"])
+		m = match(r"n=(\d+)", x[1])
+		if m != nothing 
+		    n = parse(Int, m.captures[1])
+		    x[2]["times"] ./= n
+		end
+	end
+	open(joinpath(results_dir, file), "w") do f
+	    JSON.print(f, data) 
+	end
 end
