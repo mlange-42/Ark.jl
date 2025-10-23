857dac7416ac9006246ed39364c1c3ab9fe5dd94
mlange-42 <martin_lange_@gmx.net>
2025-10-23 14:26:19 +0200

    print results

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -1,3 +1,13 @@
+using Printf
+
 include("benchmarks.jl")
 
-println(median(SUITE))
+sorted_keys = sort(collect(keys(SUITE)))
+
+for name in sorted_keys
+    bench = SUITE[name]
+    n = parse(Int, split(name, "n=")[end])
+    mean_secs = median(map(s -> s.time, bench.samples))
+    ns_per_n = 1e9 * mean_secs / n
+    @printf("%-50s %6.2fns\n", name, ns_per_n)
+end
