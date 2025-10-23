1e5be7abcd68a7bb3ed381f7f17754dc8d4712f1
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-24 00:32:26 +0200

    Update column.jl

--- src/column.jl
+++ src/column.jl
@@ -17,11 +17,11 @@ function _new_column(::Type{C}) where {C}
     Column{C}()
 end
 
-@propagate_inbounds function Base.getindex(c::Column, i::Integer)
+Base.@propagate_inbounds function Base.getindex(c::Column, i::Integer)
     return Base.getindex(c._data, i)
 end
 
-@propagate_inbounds function Base.setindex!(c::Column, value, i::Integer)
+Base.@propagate_inbounds function Base.setindex!(c::Column, value, i::Integer)
     Base.setindex!(c._data, value, i)
 end
 
@@ -52,7 +52,7 @@ function _new_entities_column()
     Entities()
 end
 
-@propagate_inbounds function Base.getindex(c::Entities, i::Integer)
+Base.@propagate_inbounds function Base.getindex(c::Entities, i::Integer)
     getindex(c._data, i)
 end
 
