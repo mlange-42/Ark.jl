93a16e647599b4863a7a6152b3542a8e8a0eea7e
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-10-24 23:57:53 +0200

    Use @inbounds in internal code (#74)

--- src/util.jl
+++ src/util.jl
@@ -3,7 +3,7 @@ function _swap_remove!(v::Vector, i::UInt32)::Bool
     last_index = length(v)
     swapped = i != last_index
     if swapped
-        v[i] = v[last_index]
+        @inbounds v[i] = v[last_index]
     end
     pop!(v)
     return swapped
