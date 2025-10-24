2b9148bd411ee43ecbc8b92c94d943ea1393dc43
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-10-24 20:02:54 +0200

    Move logic for moving components to storages (#71)

--- src/storage.jl
+++ src/storage.jl
@@ -6,3 +6,15 @@ end
 function _ComponentStorage{C}(archetypes::Int) where C
     _ComponentStorage{C}(Vector{Union{Nothing,Column{C}}}(nothing, archetypes))
 end
+
+function _move_component_data!(s::_ComponentStorage{C}, old_arch::UInt32, new_arch::UInt32, row::UInt32) where C
+    old_vec = s.data[Int(old_arch)]
+    new_vec = s.data[Int(new_arch)]
+    push!(new_vec._data, old_vec[row])
+    _swap_remove!(old_vec._data, row)
+end
+
+function _remove_component_data!(s::_ComponentStorage{C}, arch::UInt32, row::UInt32) where C
+    col = s.data[Int(arch)]
+    _swap_remove!(col._data, row)
+end
