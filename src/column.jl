fdc421fb86cafb9ba5d035b94917dd4534c7e43d
mlange-42 <martin_lange_@gmx.net>
2025-10-24 11:29:30 +0200

    generated constructor

--- src/column.jl
+++ src/column.jl
@@ -7,14 +7,39 @@ Can be iterated, indexed and updated like a Vector.
 
 Used in query iteration.
 """
-struct Column{C}
-    _data::StructArray{C}
+struct Column{C,S<:StructArray{C}}
+    _data::S
+end
 
-    Column{C}() where {C} = new(StructArray{C}(undef, 0))
+@generated function Column{C}() where {C}
+    fnames = fieldnames(C)
+    ftypes = C.types
+
+    # Build Tuple type: Tuple{Vector{T1}, Vector{T2}, ...}
+    tuple_type_expr = Expr(:curly, :Tuple, [:(Vector{$(ftypes[i])}) for i in 1:length(fnames)]...)
+
+    # Build NamedTuple type: NamedTuple{(:x, :y), Tuple{...}}
+    nt_type_expr = Expr(:curly, :NamedTuple,
+        Expr(:tuple, map(QuoteNode, fnames)...),
+        tuple_type_expr
+    )
+
+    # Build value tuple: (Vector{T1}(), Vector{T2}(), ...)
+    value_expr = Expr(:tuple, [:(Vector{$(ftypes[i])}()) for i in 1:length(fnames)]...)
+
+    return quote
+        storage = convert($nt_type_expr, $value_expr)
+        sa = StructArray{C}(storage)
+        Column{C,typeof(sa)}(sa)
+    end
 end
 
 function _new_column(::Type{C}) where {C}
-    Column{C}()
+    fnames = fieldnames(C)
+    ftypes = C.types
+    storage = NamedTuple{fnames}(ntuple(i -> Vector{ftypes[i]}(), length(fnames)))
+    sa = StructArray{C}(storage)
+    Column{C,typeof(sa)}(sa)
 end
 
 Base.@propagate_inbounds function Base.getindex(c::Column, i::Integer)
