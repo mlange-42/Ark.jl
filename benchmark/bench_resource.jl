54d189120b967a161010de17847466ab4e9e829b
tortar <adrianomeligrana1999@gmail.com>
2025-10-26 01:52:44 +0200

    bench

---
+++ benchmark/bench_resource.jl
@@ -0,0 +1,15 @@
+
+function setup_resource()
+    world = World(Position, Velocity)
+    add_resource!(world, Tick(0))
+    return world
+end
+
+function benchmark_resource(args)
+    world = args
+    res = get_resource(world)
+    res.time += 1
+    return world
+end
+
+SUITE["benchmark_resource n=$n"] = @be setup_resource() benchmark_resource(_) seconds = SECONDS
