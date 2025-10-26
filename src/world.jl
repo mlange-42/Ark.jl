e4c6843dbb4f7410981bb020514c196dae44aafd
mlange-42 <martin_lange_@gmx.net>
2025-10-26 23:05:15 +0100

    add types to local variables

--- src/world.jl
+++ src/world.jl
@@ -617,17 +617,17 @@ end
 end
 
 function _ensure_column_size_for_comp!(world::World, comp::UInt8, arch::UInt32, needed::UInt32)
-    storage = world._storages[Int(comp)]
+    storage::_ComponentStorage = world._storages[Int(comp)]
     _ensure_column_size!(storage, arch, needed)
 end
 
 function _move_component_data!(world::World, comp::UInt8, old_arch::UInt32, new_arch::UInt32, row::UInt32)
-    storage = world._storages[Int(comp)]
+    storage::_ComponentStorage = world._storages[Int(comp)]
     _move_component_data!(storage, old_arch, new_arch, row)
 end
 
 function _swap_remove_in_column_for_comp!(world::World, comp::UInt8, arch::UInt32, row::UInt32)
-    storage = world._storages[Int(comp)]
+    storage::_ComponentStorage = world._storages[Int(comp)]
     _remove_component_data!(storage, arch, row)
 end
 
