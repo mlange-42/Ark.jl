2714ee7dd76bb3e77b9c66651a94d9de2a45047c
mlange-42 <martin_lange_@gmx.net>
2025-10-26 17:58:30 +0100

    add benchmark for setting components

---
+++ benchmark/bench_world_set_1.jl
@@ -0,0 +1,24 @@
+
+function setup_world_set_1(n_entities::Int)
+    world = World(Position, Velocity)
+    map = Map(world, Val.((Position,)))
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(map, (Position(i, i * 2),))
+        push!(entities, e)
+    end
+
+    return (entities, world)
+end
+
+function benchmark_world_set_1(args, n)
+    entities, world = args
+    for e in entities
+        set_components!(world, e, (Position(1, 2),))
+    end
+end
+
+for n in (100, 1_000, 10_000, 100_000)
+    SUITE["benchmark_world_set_1 n=$n"] = @be setup_world_set_1($n) benchmark_world_set_1(_, $n) seconds = SECONDS
+end
