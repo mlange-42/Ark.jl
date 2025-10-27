2a42125904c8cdd4277030e5639985e47304fa25
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-28 00:48:27 +0100

    Update bench_world_new_entities_1.jl

--- benchmark/bench_world_new_entities_1.jl
+++ benchmark/bench_world_new_entities_1.jl
@@ -3,10 +3,16 @@ function setup_world_new_entities_1(n::Int)
     world = World(Position, Velocity)
 
     # Run once to allocate memory
+    entities = Vector{Entity}()
     for (e, pos_col) in @new_entities!(world, n, (Position,))
+        append!(entities, e)
         @inbounds for i in eachindex(e)
             pos_col[i] = Position(0, 0)
         end
+    end
+
+    for e in entities
+        remove_entity!(world, e)
     end
 
     return world
