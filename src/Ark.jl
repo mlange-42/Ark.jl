11f4281bcf8ec6f8117ee3afca50f908bf6373ee
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-27 17:21:11 +0100

    Remove internal exports (#113)

--- src/Ark.jl
+++ src/Ark.jl
@@ -24,47 +24,15 @@ export get_components, @get_components, set_components!, has_components, @has_co
 export add_components!, remove_components!, @remove_components!
 export exchange_components!, @exchange_components!
 export get_resource, has_resource, add_resource!, remove_resource!
-export _find_or_create_archetype!, _create_entity!, _move_entity!, _get_storage, _get_storage_by_id, _component_id
 
 export Entity
-export is_zero, _new_entity, _EntityIndex, _ComponentStorage
+export is_zero
 
 export Map, @Map
-export _get_mapped_components
 
 export Query, @Query
 export close!
 
 export Column, Entities
-export _new_column, _new_entities_column
-
-export _Archetype
-export _add_entity!
-
-export _ComponentRegistry
-export _get_id!, _register_component!
-
-export _Mask
-export _get_bit, _contains_all, _contains_any, _and, _or, _clear_bits, _active_bit_indices
-
-export _MutableMask
-export _get_bit, _set_bit!, _clear_bit!
-
-export _EntityPool
-export _get_entity, _recycle, _is_alive
-
-export _BitPool
-export _get_bit
-
-export _Lock
-export _lock, _unlock, _is_locked
-
-export _ComponentStorage
-
-export _VecMap
-export _get_map, _set_map!
-
-export _Graph, _GraphNode
-export _find_node, _find_or_create
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,20 @@
 
 using Ark
+using Ark: _find_or_create_archetype!, _create_entity!, _move_entity!, _get_storage, _get_storage_by_id, _component_id
+using Ark: _new_entity, _EntityIndex, _ComponentStorage
+using Ark: _get_mapped_components
+using Ark: _new_column, _new_entities_column
+using Ark: _Archetype, _add_entity!
+using Ark: _ComponentRegistry, _get_id!, _register_component!
+using Ark: _Mask, _get_bit, _contains_all, _contains_any, _and, _or, _clear_bits, _active_bit_indices
+using Ark: _MutableMask, _get_bit, _set_bit!, _clear_bit!
+using Ark: _EntityPool, _get_entity, _recycle, _is_alive
+using Ark: _BitPool, _get_bit
+using Ark: _Lock, _lock, _unlock, _is_locked
+using Ark: _ComponentStorage
+using Ark: _VecMap, _get_map, _set_map!
+using Ark: _Graph, _GraphNode, _find_node, _find_or_create
+
 using Test
 
 include("TestTypes.jl")
