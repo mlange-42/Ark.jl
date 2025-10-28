c84a23c8d7f4e3713467888419da80b9886df562
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-10-28 09:51:42 +0100

    Add benchmarks for updating components (get + set) (#122)

---
+++ benchmark/bench_world_update_5.jl
@@ -0,0 +1,31 @@
+
+function setup_world_update_5(n_entities::Int)
+    world = World(Position, Velocity, CompA, CompB, CompC)
+    map = Map(world, Val.((Position, Velocity, CompA, CompB, CompC)))
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(map, (Position(i, i * 2), Velocity(0, 0), CompA(0, 0), CompB(0, 0), CompC(0, 0)))
+        push!(entities, e)
+    end
+
+    for e in entities
+        pos, vel, a, b, c = get_components(world, e, Val.((Position, Velocity, CompA, CompB, CompC)))
+        set_components!(world, e, (Position(pos.x + 1, 0), Velocity(vel.dx + 1, 0), CompA(a.x + 1, 0), CompB(b.x + 1, 0), CompC(c.x + 1, 0)))
+    end
+
+    return (entities, world)
+end
+
+function benchmark_world_update_5(args, n)
+    entities, world = args
+    sum = 0.0
+    for e in entities
+        pos, vel, a, b, c = get_components(world, e, Val.((Position, Velocity, CompA, CompB, CompC)))
+        set_components!(world, e, (Position(pos.x + 1, 0), Velocity(vel.dx + 1, 0), CompA(a.x + 1, 0), CompB(b.x + 1, 0), CompC(c.x + 1, 0)))
+    end
+end
+
+for n in (100, 10_000)
+    SUITE["benchmark_world_update_5 n=$n"] = @be setup_world_update_5($n) benchmark_world_update_5(_, $n) seconds = SECONDS
+end
