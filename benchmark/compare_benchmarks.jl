429ac87af6f0b6acd972ad82977604cde64b3263
mlange-42 <martin_lange_@gmx.net>
2025-10-28 12:09:34 +0100

    fix read vector

--- benchmark/compare_benchmarks.jl
+++ benchmark/compare_benchmarks.jl
@@ -2,14 +2,14 @@
 include("util.jl")
 
 data_current = [
-    read_bench_table("bench_current_1.csv")
-    read_bench_table("bench_current_2.csv")
-    read_bench_table("bench_current_3.csv")
+    read_bench_table("bench_current_1.csv"),
+    read_bench_table("bench_current_2.csv"),
+    read_bench_table("bench_current_3.csv"),
 ]
 data_main = [
-    read_bench_table("bench_main_1.csv")
-    read_bench_table("bench_main_2.csv")
-    read_bench_table("bench_main_3.csv")
+    read_bench_table("bench_main_1.csv"),
+    read_bench_table("bench_main_2.csv"),
+    read_bench_table("bench_main_3.csv"),
 ]
 
 result = compare_multi_tables(data_main, data_current)
