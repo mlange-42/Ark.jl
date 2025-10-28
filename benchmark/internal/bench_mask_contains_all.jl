53368c61d2d617ab2e6c8fdc9a7127eb717f4d88
mlange-42 <martin_lange_@gmx.net>
2025-10-28 15:22:04 +0100

    Add benchmarks for masks in small worlds

--- benchmark/internal/bench_mask_contains_all.jl
+++ benchmark/internal/bench_mask_contains_all.jl
@@ -22,3 +22,26 @@ function benchmark_mask_contains_all(args, n)
 end
 
 SUITE["benchmark_mask_contains_all n=1000"] = @be setup_mask_contains_all(1000) benchmark_mask_contains_all(_, 1000) seconds = SECONDS
+
+function setup_mask_contains_all_small(n::Int)
+    mask_a = _Mask(1)
+    masks = [
+        _Mask(1),
+        _Mask(1),
+        _Mask(2),
+    ]
+
+    return mask_a, masks
+end
+
+function benchmark_mask_contains_all_small(args, n)
+    mask_a, masks = args
+    len = length(masks)
+    b = false
+    for i in 1:n
+        b = _contains_all(mask_a, masks[i%len+1])
+    end
+    return b
+end
+
+SUITE["benchmark_mask_contains_all_small n=1000"] = @be setup_mask_contains_all_small(1000) benchmark_mask_contains_all_small(_, 1000) seconds = SECONDS
--- benchmark/internal/bench_mask_contains_any.jl
+++ benchmark/internal/bench_mask_contains_any.jl
@@ -22,3 +22,26 @@ function benchmark_mask_contains_any(args, n)
 end
 
 SUITE["benchmark_mask_contains_any n=1000"] = @be setup_mask_contains_any(1000) benchmark_mask_contains_any(_, 1000) seconds = SECONDS
+
+function setup_mask_contains_any_small(n::Int)
+    mask_a = _Mask(1)
+    masks = [
+        _Mask(2),
+        _Mask(1),
+        _Mask(2),
+    ]
+
+    return mask_a, masks
+end
+
+function benchmark_mask_contains_any_small(args, n)
+    mask_a, masks = args
+    len = length(masks)
+    b = false
+    for i in 1:n
+        b = _contains_any(mask_a, masks[i%len+1])
+    end
+    return b
+end
+
+SUITE["benchmark_mask_contains_any_small n=1000"] = @be setup_mask_contains_any_small(1000) benchmark_mask_contains_any_small(_, 1000) seconds = SECONDS
