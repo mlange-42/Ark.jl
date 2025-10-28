15c3b0db1b94eaa5e8f5b42e2f6bedd7ae77ef88
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-10-28 16:07:22 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,13 +5,22 @@ DocMeta.setdocmeta!(Ark, :DocTestSetup, :(using Ark); recursive=true)
 
 makedocs(;
     modules=[Ark],
-    authors="Martin Lange <martin_lange_@gmx.net>",
+    authors = "Martin Lange <martin_lange_@gmx.net>, Adriano Meligrana <adriano.meligrana@centai.eu>"
     sitename="Ark.jl",
     format=Documenter.HTML(;
+        canonical="https://mlange-42.github.io/Ark.jl",
         edit_link="main",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
+        "API" => "api.md"
     ],
+    warnonly = [:missing_docs],
+)
+
+deploydocs(;
+    repo="github.com/mlange-42/Ark.jl",
+    devbranch="main",
+    push_preview = true,
 )
