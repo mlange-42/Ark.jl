74d74c7026f76ef7823b9af8ab968dc8a89ff8d4
mlange-42 <martin_lange_@gmx.net>
2025-10-29 14:05:45 +0100

    more general setter

--- src/util.jl
+++ src/util.jl
@@ -9,10 +9,9 @@ function _swap_remove!(v::Vector, i::UInt32)::Bool
     return swapped
 end
 
-struct _Missing{C}
-end
+struct _Missing{C} end
 
 Base.getindex(::_Missing{C}, i::Integer) where C = error(error("entity has no $(string(C)) component"))
-Base.setindex!(::_Missing{C}, ::C, ::Integer) where C = error(error("entity has no $(string(C)) component"))
+Base.setindex!(::_Missing{C}, value, ::Integer) where C = error(error("entity has no $(string(C)) component"))
 Base.length(::_Missing{C}) where C = 0
 Base.iterate(::_Missing{C}) where C = nothing
