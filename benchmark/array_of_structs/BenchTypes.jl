4f4cba25f140f9beb952f42e96b016b3de686844
mlange-42 <martin_lange_@gmx.net>
2025-10-31 00:57:34 +0100

    add immutable

--- benchmark/array_of_structs/BenchTypes.jl
+++ benchmark/array_of_structs/BenchTypes.jl
@@ -105,3 +105,64 @@ end
 function AosFlat_128B()
     AosFlat_128B(0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 end
+
+struct AosImmutable_32B
+    x::Float64
+    y::Float64
+    dx::Float64
+    dy::Float64
+end
+
+function AosImmutable_32B()
+    AosImmutable_32B(0, 0, 1, 1)
+end
+
+function AosImmutable_32B(x::Float64, y::Float64)
+    AosImmutable_32B(x, y, 1, 1)
+end
+
+struct AosImmutable_64B
+    x::Float64
+    y::Float64
+    dx::Float64
+    dy::Float64
+    p1::Float64
+    p2::Float64
+    p3::Float64
+    p4::Float64
+end
+
+function AosImmutable_64B()
+    AosImmutable_64B(0, 0, 1, 1, 0, 0, 0, 0)
+end
+
+function AosImmutable_64B(x::Float64, y::Float64)
+    AosImmutable_64B(x, y, 1, 1, 0, 0, 0, 0)
+end
+
+struct AosImmutable_128B
+    x::Float64
+    y::Float64
+    dx::Float64
+    dy::Float64
+    p1::Float64
+    p2::Float64
+    p3::Float64
+    p4::Float64
+    p5::Float64
+    p6::Float64
+    p7::Float64
+    p8::Float64
+    p9::Float64
+    p10::Float64
+    p11::Float64
+    p12::Float64
+end
+
+function AosImmutable_128B()
+    AosImmutable_128B(0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+end
+
+function AosImmutable_128B(x::Float64, y::Float64)
+    AosImmutable_128B(x, y, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+end
