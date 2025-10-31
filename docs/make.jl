4aba89e6fe3be124bfdc4c43fbf7d237740aa550
mlange-42 <martin_lange_@gmx.net>
2025-10-31 20:28:42 +0100

    fix format

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using Ark
 using Documenter
 
-DocMeta.setdocmeta!(Ark, :DocTestSetup, :(using Ark); recursive=true)
+DocMeta.setdocmeta!(Ark, :DocTestSetup, :(using Ark); recursive = true)
 
 doctest(Ark)
 
 makedocs(;
-    modules=[Ark],
-    sitename="Ark.jl",
-    authors="Martin Lange <martin_lange_@gmx.net>, Adriano Meligrana <adriano.meligrana@centai.eu>",
-    format=Documenter.HTML(;
-        description="Ark.jl is an archetype-based entity component system (ECS) for Julia.",
-        canonical="https://mlange-42.github.io/Ark.jl/dev",
-        edit_link="main",
-        prettyurls=false,
-        sidebar_sitename=false,
-        assets=["assets/favicon.ico", "assets/custom.css"],
+    modules = [Ark],
+    sitename = "Ark.jl",
+    authors = "Martin Lange <martin_lange_@gmx.net>, Adriano Meligrana <adriano.meligrana@centai.eu>",
+    format = Documenter.HTML(;
+        description = "Ark.jl is an archetype-based entity component system (ECS) for Julia.",
+        canonical = "https://mlange-42.github.io/Ark.jl/dev",
+        edit_link = "main",
+        prettyurls = false,
+        sidebar_sitename = false,
+        assets = ["assets/favicon.ico", "assets/custom.css"],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
         "Manual" => [
             "manual/quickstart.md",
@@ -30,14 +30,14 @@ makedocs(;
             "manual/architecture.md",
         ],
         "Public API" => "api.md",
-        "Benchmarks" => "benchmarks.md"
+        "Benchmarks" => "benchmarks.md",
     ],
-    warnonly=false,
+    warnonly = false,
 )
 
 deploydocs(;
-    repo="github.com/mlange-42/Ark.jl.git",
-    versions=[
+    repo = "github.com/mlange-42/Ark.jl.git",
+    versions = [
         "stable" => "v^",
         "dev" => "main",
     ],
