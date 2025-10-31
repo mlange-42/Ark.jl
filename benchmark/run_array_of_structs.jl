0cac20ffa479c577bfec186d47a9b30405756c8c
mlange-42 <martin_lange_@gmx.net>
2025-10-31 15:24:37 +0100

    plot results

--- benchmark/run_array_of_structs.jl
+++ benchmark/run_array_of_structs.jl
@@ -1,11 +1,17 @@
 
 include("util/process.jl")
 include("util/compare.jl")
+include("util/plot.jl")
+
+"""
 include("array_of_structs/benchmarks.jl")
 
 result = process_benches_aos(SUITE)
-write_bench_table_aos(result, "bench.csv")
+write_bench_table_aos(result, "bench_aos.csv")
 
 for r in result
     @printf("%-20s  %3dB  %7d  %6.2fns\n", r.name, r.bytes, r.n, r.time_ns)
 end
+"""
+
+plot_aos("bench_aos.csv", "bench_aos.svg")
---
+++ benchmark/util/plot.jl
@@ -0,0 +1,24 @@
+using DataFrames
+using CSV
+using Plots
+
+function plot_aos(in_file::String, out_file::String)
+    df_raw = CSV.read(in_file, DataFrame)
+    df = unstack(df_raw, [:Bytes, :Vars, :N], :Name, :Time)
+
+    plt = plot(xscale=:log10, xlabel="Number of entities", ylabel="Time per entity [ns]", title="Benchmark vs. Array of Structs")
+
+    sizes = unique(select(df, [:Bytes, :Vars]))
+
+    for (impl, color, label) in [(Symbol("benchmark_ark"), :blue, "Ark"), (Symbol("benchmark_outer"), :red, "AoS")]
+        for row in eachrow(sizes)
+            bytes = row.Bytes
+            vars = row.Vars
+            subset = filter(r -> r.Bytes == bytes && r.Vars == vars, df)
+            label_ark = "$label ($(bytes)B / $(vars) Vars)"
+            plot!(subset.N, subset[!, impl], label=label_ark, lw=0.3 + 0.5 * log2(vars), color=color)
+        end
+    end
+
+    savefig(out_file)
+end
