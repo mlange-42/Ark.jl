1f25fc93502e6217c55c00f043d54479b0460f15
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-02 03:46:42 +0100

    Update util.jl

--- src/util.jl
+++ src/util.jl
@@ -9,7 +9,7 @@ function _swap_remove!(v::Vector, i::UInt32)::Bool
     return swapped
 end
 
-const DEBUG = isdefined(Main, :__RUNNING_TESTS__) && Main.__RUNNING_TESTS__ === true
+const DEBUG = ("ARK_RUNNING_TESTS" in ENV && ENV["ARK_RUNNING_TESTS"])
 
 macro check(arg)
     DEBUG ? esc(:(@assert $arg)) : nothing
