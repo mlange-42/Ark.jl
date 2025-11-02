a8f0d5cd3b0a7aebeeda3b269d2e735a1367a1b4
mlange-42 <martin_lange_@gmx.net>
2025-11-02 01:08:30 +0100

    complete add/remove observer

--- src/event.jl
+++ src/event.jl
@@ -69,18 +69,64 @@ function _add_observer!(m::_EventManager, o::Observer)
     if o._id.id > 0
         error("observer is already registered")
     end
-    push!(m.observers[o._event._id], o)
-    o._id.id = UInt32(length(m.observers[o._event._id]))
+    e = o._event._id
+    push!(m.observers[e], o)
+    o._id.id = UInt32(length(m.observers[e]))
+
+    if o._has_with
+        m.union_with[e] = _or(m.union_with[e], o._with)
+    else
+        m.any_no_with[e] = true
+    end
+
+    if e == OnCreateEntity || e == OnRemoveEntity
+        return
+    end
+
+    if o._has_comps
+        m.union_comps[e] = _or(m.union_comps[e], o._comps)
+    else
+        m.any_no_comps[e] = true
+    end
 end
 
 function _remove_observer!(m::_EventManager, o::Observer)
     if o._id.id == 0
         error("observer is not registered")
     end
-    observers = m.observers[o._event._id]
+    e = o._event._id
+    observers = m.observers[e]
     swapped = _swap_remove!(observers, o._id.id)
     if swapped
         observers[o._id.id]._id.id = o._id.id
     end
     o._id.id = 0
+
+    # rebuild mask unions
+
+    m.any_no_with[e] = false
+    with_mask = _Mask()
+    for o in m.observers[e]
+        if !o._has_with
+            m.any_no_with[e] = true
+            break # skip, as the unions mask is irrelevant
+        end
+        with_mask = _or(with_mask, o._with)
+    end
+    m.union_with[e] = with_mask
+
+    if e == OnCreateEntity || e == OnRemoveEntity
+        return
+    end
+
+    m.any_no_comps[e] = false
+    comps_mask = _Mask()
+    for o in m.observers[e]
+        if !o._has_comps
+            m.any_no_comps[e] = true
+            break # skip, as the unions mask is irrelevant
+        end
+        comps_mask = _or(comps_mask, o._comps)
+    end
+    m.union_comps[e] = comps_mask
 end
