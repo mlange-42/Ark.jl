a5d0f0f5f347611af00e7c283eb68df783a06a94
mlange-42 <martin_lange_@gmx.net>
2025-11-03 13:17:37 +0100

    use type check instead of try/catch

--- src/util.jl
+++ src/util.jl
@@ -10,14 +10,13 @@ function _swap_remove!(v::Vector, i::UInt32)::Bool
 end
 
 @inline function _try_to_types(::Type{TS}) where {TS<:Tuple}
-    try
-        return [x.parameters[1] for x in TS.parameters]
-    catch
+    if !all(x -> x <: Val, TS.parameters)
         error(
             lazy"expected a tuple of Val types like Val.((Position, Velocity)), got $TS. " *
             "Consider using the related macro instead.",
         )
     end
+    return [x.parameters[1] for x in TS.parameters]
 end
 
 const DEBUG = ("ARK_RUNNING_TESTS" in keys(ENV) && lowercase(ENV["ARK_RUNNING_TESTS"]) == "true")
