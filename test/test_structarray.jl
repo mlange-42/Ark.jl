55494338ba385991decc88f08434180e2ab7650d
mlange-42 <martin_lange_@gmx.net>
2025-11-04 17:37:01 +0100

    implement push, pop and resize

--- test/test_structarray.jl
+++ test/test_structarray.jl
@@ -7,10 +7,36 @@
 
     push!(a, Position(1, 2))
 
+    @test length(a) == 1
+    @test size(a) == (1,)
     @test length(a.components.x) == 1
     @test length(a.components.y) == 1
     @test a[1] == Position(1, 2)
 
     a[1] = Position(3, 4)
     @test a[1] == Position(3, 4)
+
+    push!(a, Position(5, 6))
+    @test length(a) == 2
+
+    pop!(a)
+    @test length(a) == 1
+
+    resize!(a, 10)
+    @test length(a) == 10
+
+    for i in 1:10
+        a[i] = Position(i, i)
+    end
+    for i in 1:10
+        @test a[i] == Position(i, i)
+    end
+end
+
+@testset "StructArray no fields" begin
+    a = _StructArray(LabelComponent)
+
+    push!(a, LabelComponent())
+    @test length(a) == 1
+    a[1] = LabelComponent()
 end
