353b981e5dc82114953395fc769243eb48bf5dac
mlange-42 <martin_lange_@gmx.net>
2025-11-04 17:38:00 +0100

    add iteration benchmark

---
+++ benchmark/internal/bench_structarray.jl
@@ -0,0 +1,41 @@
+using Ark: _StructArray
+
+function setup_iterate_vector(n::Int)
+    arr = Vector{Position}()
+    for i in 1:n
+        push!(arr, Position(i, i))
+    end
+    return arr
+end
+
+function benchmark_iterate_vector(args, n::Int)
+    arr = args
+    sum = 0.0
+    for pos in arr
+        sum += pos.x
+    end
+    return sum
+end
+
+SUITE["benchmark_iterate_vector n=1000"] =
+    @be setup_iterate_vector(1000) benchmark_iterate_vector(_, 1000) seconds = SECONDS
+
+function setup_iterate_structarray(n::Int)
+    arr = _StructArray(Position)
+    for i in 1:n
+        push!(arr, Position(i, i))
+    end
+    return arr
+end
+
+function benchmark_iterate_structarray(args, n::Int)
+    arr = args
+    sum = 0.0
+    for pos in arr
+        sum += pos.x
+    end
+    return sum
+end
+
+SUITE["benchmark_iterate_structarray n=1000"] =
+    @be setup_iterate_structarray(1000) benchmark_iterate_structarray(_, 1000) seconds = SECONDS
