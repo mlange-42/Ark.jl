2c9cac78eaec25f7dc367d66833d047808041bc0
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-11-04 20:56:33 +0100

    SubArray/view for StructArray, more functionality (#206)

--- src/structarray.jl
+++ src/structarray.jl
@@ -21,6 +21,18 @@ end
     end
 end
 
+@generated function _StructArray_type(::Type{C}) where {C}
+    names = fieldnames(C)
+    types = fieldtypes(C)
+    num_fields = length(types)
+
+    nt_type = :(NamedTuple{($(map(QuoteNode, names)...),),Tuple{$(map(t -> :(Vector{$t}), types)...)}})
+
+    return quote
+        _StructArray{C,$nt_type,$num_fields}
+    end
+end
+
 @generated function Base.getproperty(sa::_StructArray{C}, name::Symbol) where {C}
     # TODO: do we need this? Seems not called when doing `sa.components`.
     #if name == :components || name == :length
@@ -59,6 +71,14 @@ end
     ]
     dec_length = :(sa.length -= 1)
     return Expr(:block, pop_exprs..., dec_length, :(sa))
+end
+
+@generated function Base.fill!(sa::_StructArray{C}, value::C) where {C}
+    names = fieldnames(C)
+    fill_exprs = [
+        :(fill!(sa.components.$name, value.$name)) for name in names
+    ]
+    return Expr(:block, fill_exprs..., :(sa))
 end
 
 @generated function Base.getindex(sa::_StructArray{C}, i::Int) where {C}
@@ -100,3 +120,64 @@ function Base.firstindex(sa::_StructArray)
 end
 
 Base.lastindex(sa::_StructArray) = sa.length
+
+struct _StructArrayView{C,CS<:NamedTuple,N,I} <: AbstractArray{C,1}
+    components::CS
+    indices::I
+end
+
+@generated function Base.view(
+    sa::S,
+    idx::I,
+) where {S<:_StructArray{C,CS,N},I<:AbstractUnitRange{Int}} where {C,CS<:NamedTuple,N}
+    names = fieldnames(C)
+    types = fieldtypes(C)
+    view_exprs = [
+        :($name = @view sa.components.$name[idx]) for name in names
+    ]
+    nt_type = :(NamedTuple{
+        ($(map(QuoteNode, names)...),),
+        Tuple{$(map(t -> :(SubArray{$t,1,Vector{$t},Tuple{I},true}), types)...)},
+    })
+    return quote
+        _StructArrayView{C,$nt_type,length($names),I}((; $(view_exprs...)), idx)
+    end
+end
+
+@generated function Base.getindex(sa::_StructArrayView{C}, i::Int) where {C}
+    names = fieldnames(C)
+    field_exprs = [
+        :($(name) = sa.components.$name[i]) for name in names
+    ]
+    return Expr(:block, Expr(:call, C, field_exprs...))
+end
+
+@generated function Base.setindex!(sa::_StructArrayView{C}, c::C, i::Int) where {C}
+    names = fieldnames(C)
+    set_exprs = [
+        :(sa.components.$name[i] = c.$name) for name in names
+    ]
+    return Expr(:block, set_exprs..., :(sa))
+end
+
+@generated function Base.fill!(sa::_StructArrayView{C}, value::C) where {C}
+    names = fieldnames(C)
+    fill_exprs = [
+        :(fill!(sa.components.$name, value.$name)) for name in names
+    ]
+    return Expr(:block, fill_exprs..., :(sa))
+end
+
+Base.size(sa::_StructArrayView) = (length(sa.indices),)
+Base.length(sa::_StructArrayView) = length(sa.indices)
+Base.eltype(::Type{<:_StructArrayView{C}}) where {C} = C
+Base.IndexStyle(::Type{<:_StructArrayView}) = IndexLinear()
+Base.eachindex(sa::_StructArrayView) = 1:length(sa)
+
+function Base.firstindex(sa::_StructArrayView)
+    # Do not simplify to this, as it is then not covered by the tests for some reason:
+    # Base.firstindex(sa::_StructArray) = 1
+    return 1
+end
+
+Base.lastindex(sa::_StructArrayView) = length(sa)
--- test/test_structarray.jl
+++ test/test_structarray.jl
@@ -23,6 +23,11 @@
 
     pop!(a)
     @test length(a) == 1
+
+    fill!(a, Position(99, 99))
+    for pos in a
+        @test pos == Position(99, 99)
+    end
 end
 
 @testset "StructArray iteration" begin
@@ -75,4 +80,58 @@ end
     x, y = a.components
     @test isa(x, Vector{Float64})
     @test isa(y, Vector{Float64})
+end
+
+@testset "StructArray type" begin
+    tp = _StructArray_type(Position)
+    @test tp == _StructArray{Position,@NamedTuple{x::Vector{Float64}, y::Vector{Float64}},2}
+end
+
+@testset "Vector view" begin
+    # template for tests below to ensure that StructArray views behave like Vector views
+    a = Vector{Position}()
+    for i in 1:10
+        push!(a, Position(i, i))
+    end
+
+    v = view(a, 5:10)
+    @test v[1] == Position(5, 5)
+    v[1] = Position(99, 99)
+    @test v[1] == Position(99, 99)
+
+    @test length(v) == 6
+    @test size(v) == (6,)
+    @test firstindex(v) == 1
+    @test lastindex(v) == 6
+    @test eachindex(v) == 1:6
+end
+
+@testset "StructArray view" begin
+    a = _StructArray(Position)
+    for i in 1:10
+        push!(a, Position(i, i))
+    end
+
+    v = view(a, 5:10)
+    x, y = v.components
+    @test isa(x, SubArray{Float64})
+    @test isa(y, SubArray{Float64})
+
+    @test v[1] == Position(5, 5)
+    v[1] = Position(99, 99)
+    @test v[1] == Position(99, 99)
+
+    @test length(v) == 6
+    @test size(v) == (6,)
+    @test firstindex(v) == 1
+    @test lastindex(v) == 6
+    @test eachindex(v) == 1:6
+
+    @test Base.eltype(typeof(v)) == Position
+    @test Base.IndexStyle(typeof(v)) == IndexLinear()
+
+    fill!(v, Position(99, 99))
+    for pos in v
+        @test pos == Position(99, 99)
+    end
 end
