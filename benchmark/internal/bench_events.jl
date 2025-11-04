c0c7bf45ac833a88cc0249efa84c5c982727b650
mlange-42 <martin_lange_@gmx.net>
2025-11-04 17:20:52 +0100

    set up benchmarks

---
+++ benchmark/internal/bench_events.jl
@@ -0,0 +1,112 @@
+function setup_event_no_obs(n::Int)
+    reg = EventRegistry()
+    evt = new_event_type!(reg)
+
+    world = World(Position)
+
+    entities = []
+    for _ in 1:n
+        push!(entities, new_entity!(world, (Position(0, 0),)))
+    end
+
+    return world, entities, evt
+end
+
+function benchmark_event_no_obs(args, n)
+    world, entities, evt = args
+    for entity in entities
+        @emit_event!(world, evt, entity, (Position,))
+    end
+    world
+end
+
+SUITE["benchmark_event_no_obs n=1000"] = @be setup_event_no_obs(1000) benchmark_event_no_obs(_, 1000) seconds = SECONDS
+
+function setup_event_no_match(n::Int)
+    reg = EventRegistry()
+    evt = new_event_type!(reg)
+
+    world = World(Position, Velocity)
+
+    entities = []
+    for _ in 1:n
+        push!(entities, new_entity!(world, (Position(0, 0),)))
+    end
+
+    @observe!(world, evt, (Velocity,)) do entity
+    end
+
+    return world, entities, evt
+end
+
+function benchmark_event_no_match(args, n)
+    world, entities, evt = args
+    for entity in entities
+        @emit_event!(world, evt, entity, (Position,))
+    end
+    world
+end
+
+SUITE["benchmark_event_no_match n=1000"] =
+    @be setup_event_no_match(1000) benchmark_event_no_match(_, 1000) seconds = SECONDS
+
+function setup_event_match_1(n::Int)
+    reg = EventRegistry()
+    evt = new_event_type!(reg)
+
+    world = World(Position, Velocity)
+
+    entities = []
+    for _ in 1:n
+        push!(entities, new_entity!(world, (Position(0, 0),)))
+    end
+
+    @observe!(world, evt, (Position,)) do entity
+    end
+
+    return world, entities, evt
+end
+
+function benchmark_event_match_1(args, n)
+    world, entities, evt = args
+    for entity in entities
+        @emit_event!(world, evt, entity, (Position,))
+    end
+    world
+end
+
+SUITE["benchmark_event_match_1 n=1000"] =
+    @be setup_event_match_1(1000) benchmark_event_match_1(_, 1000) seconds = SECONDS
+
+function setup_event_match_1_of_5(n::Int)
+    reg = EventRegistry()
+    evt = new_event_type!(reg)
+
+    world = World(Position, Velocity)
+
+    entities = []
+    for _ in 1:n
+        push!(entities, new_entity!(world, (Position(0, 0),)))
+    end
+
+    @observe!(world, evt, (Position,)) do entity
+    end
+
+    for _ in 1:4
+        @observe!(world, evt, (Velocity,)) do entity
+        end
+    end
+
+    return world, entities, evt
+end
+
+function benchmark_event_match_1_of_5(args, n)
+    world, entities, evt = args
+    for entity in entities
+        @emit_event!(world, evt, entity, (Position,))
+    end
+    world
+end
+
+SUITE["benchmark_event_match_1_of_5 n=1000"] =
+    @be setup_event_match_1_of_5(1000) benchmark_event_match_1_of_5(_, 1000) seconds = SECONDS
