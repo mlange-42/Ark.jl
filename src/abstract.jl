85179add2ecebb2345fcf2c403df2e986981b36a
mlange-42 <martin_lange_@gmx.net>
2025-11-05 17:24:04 +0100

    add docs example for StructArrayComponent

--- src/abstract.jl
+++ src/abstract.jl
@@ -4,5 +4,17 @@ abstract type _AbstractWorld end
     StructArrayComponent
 
 Marker trait for component types that use a StructArray-like storage.
+
+# Example
+
+```jldoctest; setup = :(using Ark), output = false
+struct Position <: StructArrayComponent
+    x::Float64
+    y::Float64
+end
+
+# output
+
+```
 """
 abstract type StructArrayComponent end
