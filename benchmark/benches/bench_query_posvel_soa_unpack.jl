8f9b4a67b26fcc2a403b642472c3ee38fe6857ce
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-05 21:59:12 +0100

    Update bench_query_posvel_soa_unpack.jl

--- benchmark/benches/bench_query_posvel_soa_unpack.jl
+++ benchmark/benches/bench_query_posvel_soa_unpack.jl
@@ -16,7 +16,7 @@ function benchmark_query_posvel_soa_unpack(args, n)
     world = args
     for columns in @Query(world, (PositionSoA, VelocitySoA))
         @unpack _, (x, y), (dx, dy) = columns
-        for i in eachindex(x, y)
+        for i in eachindex(x, y, dx, dy)
             x[i] += dx[i]
             y[i] += dy[i]
         end
