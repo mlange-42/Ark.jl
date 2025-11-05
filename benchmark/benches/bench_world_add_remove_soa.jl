f428d0ec6a139dd3a3effe28140fbf5e489dbfdf
mlange-42 <martin_lange_@gmx.net>
2025-11-05 17:24:05 +0100

    add a first benchmark

---
+++ benchmark/benches/bench_world_add_remove_soa.jl
@@ -0,0 +1,30 @@
+
+function setup_world_add_remove_soa(n_entities::Int)
+    world = World((Position, StructArrayStorage), (Velocity, StructArrayStorage))
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(world, (Position(i, i * 2),))
+        push!(entities, e)
+    end
+
+    for e in entities
+        add_components!(world, e, (Velocity(0, 0),))
+        remove_components!(world, e, Val.((Velocity,)))
+    end
+
+    return (entities, world)
+end
+
+function benchmark_world_add_remove_soa(args, n)
+    entities, world = args
+    for e in entities
+        add_components!(world, e, (Velocity(0, 0),))
+        remove_components!(world, e, Val.((Velocity,)))
+    end
+end
+
+for n in (100, 10_000)
+    SUITE["benchmark_world_add_remove_soa n=$(n)"] =
+        @be setup_world_add_remove_soa($n) benchmark_world_add_remove_soa(_, $n) seconds = SECONDS
+end
