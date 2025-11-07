f5f2c28979126f4466e054ee388bbef614e91d3c
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-07 14:58:31 +0100

    Enable simd on some mask functions

--- src/mask.jl
+++ src/mask.jl
@@ -105,17 +105,17 @@ end
 end
 
 @inline function _is_zero(m::_Mask)::Bool
-    return m.bits[1] == 0 &&
-           m.bits[2] == 0 &&
-           m.bits[3] == 0 &&
+    return m.bits[1] == 0 *
+           m.bits[2] == 0 *
+           m.bits[3] == 0 *
            m.bits[4] == 0
 end
 
 @inline function _is_not_zero(m::_Mask)::Bool
-    return m.bits[1] != 0 ||
-           m.bits[2] != 0 ||
-           m.bits[3] != 0 ||
-           m.bits[4] != 0
+    return !(m.bits[1] == 0 *
+             m.bits[2] == 0 *
+             m.bits[3] == 0 *
+             m.bits[4] == 0)
 end
 
 function _active_bit_indices(mask::_Mask)::Vector{UInt8}
