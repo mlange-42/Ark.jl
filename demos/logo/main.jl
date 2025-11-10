a790cc9806a5cd168243ce6f56443fd8aea07f2b
mlange-42 <martin_lange_@gmx.net>
2025-11-10 12:57:54 +0100

    render an image

--- demos/logo/main.jl
+++ demos/logo/main.jl
@@ -1,8 +1,14 @@
 using GLMakie
 
+size = (800, 600)
+
 GLMakie.activate!(renderloop=GLMakie.renderloop)
 
-scene = GLMakie.Scene(size=(800, 600))
+scene = GLMakie.Scene(size=size, camera=campixel!)
 screen = display(scene)
+
+img_data = rand(UInt8, size...)
+img_node = Observable(img_data)
+image!(scene, img_node)
 
 GLMakie.renderloop(screen)
