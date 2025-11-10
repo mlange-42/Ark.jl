5755e0d27f6a5d04e9f069bdac9e4c20d13765dd
mlange-42 <martin_lange_@gmx.net>
2025-11-10 13:19:16 +0100

    update loop

--- demos/logo/main.jl
+++ demos/logo/main.jl
@@ -4,11 +4,19 @@ size = (800, 600)
 
 GLMakie.activate!(renderloop=GLMakie.renderloop)
 
-scene = GLMakie.Scene(size=size, camera=campixel!)
-screen = display(scene)
+screen = GLMakie.Screen(framerate=60.0, vsync=true, render_on_demand=false, title="Ark.jl demo")
+scene = Scene(camera=campixel!, size=size)
+display(screen, scene)
 
 img_data = rand(UInt8, size...)
 img_node = Observable(img_data)
 image!(scene, img_node)
+
+on(screen.render_tick) do _
+    x = rand(1:size[1])
+    y = rand(1:size[2])
+    img_data[x, y] = 0
+    img_node[] = img_data
+end
 
 GLMakie.renderloop(screen)
