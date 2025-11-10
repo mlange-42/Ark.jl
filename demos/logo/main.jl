c2d473c7be78f3aea8e5c9f1cdf4b87a53ea7c5b
mlange-42 <martin_lange_@gmx.net>
2025-11-10 13:28:06 +0100

    measure FPS

--- demos/logo/main.jl
+++ demos/logo/main.jl
@@ -1,4 +1,5 @@
 using GLMakie
+using Dates
 
 size = (800, 600)
 
@@ -12,11 +13,18 @@ img_data = rand(UInt8, size...)
 img_node = Observable(img_data)
 image!(scene, img_node)
 
+last_time = Ref(now())
+
 on(screen.render_tick) do _
-    x = rand(1:size[1])
-    y = rand(1:size[2])
-    img_data[x, y] = 0
+    for i in 1:1000
+        x = rand(1:size[1])
+        y = rand(1:size[2])
+        img_data[x, y] = 0
+    end
     img_node[] = img_data
+    current_time = now()
+    fps = 1 / (Millisecond(current_time - last_time[]).value / 1000)
+    last_time[] = current_time
 end
 
 GLMakie.renderloop(screen)
