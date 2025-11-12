f2461ea58861991d3d29690fa366fb54612b55d6
mlange-42 <martin_lange_@gmx.net>
2025-11-12 11:21:43 +0100

    tweak benchmarks

--- benchmark/benches/bench_world_get_1.jl
+++ benchmark/benches/bench_world_get_1.jl
@@ -27,7 +27,40 @@ function benchmark_world_get_1(args, n)
     end
 end
 
+function setup_world_get_1_access(n_entities::Int)
+    world = World(Position, Velocity)
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(world, (Position(i, i * 2),))
+        push!(entities, e)
+    end
+
+    sum = 0.0
+    for e in entities
+        pos, = access_components(world, e, Val.((Position,)))
+        sum += pos[].x
+    end
+    sum
+
+    return (entities, world)
+end
+
+function benchmark_world_get_1_access(args, n)
+    entities, world = args
+    sum = 0.0
+    for e in entities
+        pos, = access_components(world, e, Val.((Position,)))
+        sum += pos[].x
+    end
+end
+
 for n in (100, 10_000)
     SUITE["benchmark_world_get_1 n=$(n)"] =
         @be setup_world_get_1($n) benchmark_world_get_1(_, $n) evals = 100 seconds = SECONDS
+end
+
+for n in (100, 10_000)
+    SUITE["benchmark_world_get_1_access n=$(n)"] =
+        @be setup_world_get_1_access($n) benchmark_world_get_1_access(_, $n) evals = 100 seconds = SECONDS
 end
--- benchmark/benches/bench_world_posvel.jl
+++ benchmark/benches/bench_world_posvel.jl
@@ -24,6 +24,25 @@ function benchmark_world_posvel(args, n)
     end
 end
 
+function setup_world_posvel_access(n_entities::Int)
+    world = World(Position, Velocity)
+
+    entities = Vector{Entity}()
+    for i in 1:n_entities
+        e = new_entity!(world, (Position(i, i * 2), Velocity(1, 1)))
+        push!(entities, e)
+    end
+
+    for e in entities
+        pos_acc, vel_acc = access_components(world, e, Val.((Position, Velocity)))
+        pos = pos_acc[]
+        vel = vel_acc[]
+        pos_acc[] = Position(pos.x + vel.dx, pos.y + vel.dy)
+    end
+
+    return (entities, world)
+end
+
 function benchmark_world_posvel_access(args, n)
     entities, world = args
     for e in entities
@@ -41,5 +60,5 @@ end
 
 for n in (100, 1_000, 10_000, 100_000)
     SUITE["benchmark_world_posvel_access n=$(n)"] =
-        @be setup_world_posvel($n) benchmark_world_posvel_access(_, n) evals = 100 seconds = SECONDS
+        @be setup_world_posvel_access($n) benchmark_world_posvel_access(_, n) evals = 100 seconds = SECONDS
 end
