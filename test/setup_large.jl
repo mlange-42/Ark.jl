b0b58df9972997d96d37bff535cf6f8b8500b9fd
mlange-42 <martin_lange_@gmx.net>
2025-11-13 00:52:47 +0100

    fix argument type

--- test/setup_large.jl
+++ test/setup_large.jl
@@ -1,5 +1,5 @@
 
-function Ark.World(comp_types::Union{Type,Pair{<:Type,<:Type}}...; initial_capacity=1024, allow_mutable=false)
+function Ark.World(comp_types::Union{Type,Pair{<:Type,<:Type}}...; initial_capacity::Int=128, allow_mutable=false)
     types = map(arg -> arg isa Type ? arg : arg.first, comp_types)
     storages = map(arg -> arg isa Type ? VectorStorage : arg.second, comp_types)
     Ark._World_from_types(
