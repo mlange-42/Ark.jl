1f3bc6cceadfeac7d6422a232ab3376d59ad9b3f
mlange-42 <martin_lange_@gmx.net>
2025-11-12 16:14:59 +0100

    simplify mouse

--- demos/logo/sys/mouse.jl
+++ demos/logo/sys/mouse.jl
@@ -2,24 +2,18 @@ struct MouseSystem <: System
 end
 
 function initialize!(s::MouseSystem, world::World)
-    screen = get_resource(world, WorldScreen)
     add_resource!(world, Mouse(0, 0, false))
+end
 
-    function mouse_move(window::Ptr{Cvoid}, x::Cint, y::Cint)::Cvoid
-        ptr = mfb_get_user_data(window)
-        w = unsafe_pointer_to_objref(ptr)::World
+function update!(s::MouseSystem, world::World)
+    screen = get_resource(world, WorldScreen)
+    mouse = get_resource(world, Mouse)
+    size = get_resource(world, WorldSize)
+    scale = get_resource(world, Scale)
 
-        mouse = get_resource(w, Mouse)
-        size = get_resource(w, WorldSize)
-        scale = get_resource(w, Scale)
+    mouse.x = mfb_get_mouse_x(screen.screen) * scale.scale
+    mouse.y = mfb_get_mouse_y(screen.screen) * scale.scale
 
-        sx, sy = x * scale.scale, y * scale.scale
-        mouse.x = sx
-        mouse.y = sy
-        mouse.inside = contains(size, Float64(sx), Float64(sy))
-
-        return nothing
-    end
-
-    mfb_set_mouse_move_callback(screen.screen, mouse_move)
+    # TODO: this is currently useless, as coords are always inside the window
+    mouse.inside = contains(size, mouse.x, mouse.y)
 end
