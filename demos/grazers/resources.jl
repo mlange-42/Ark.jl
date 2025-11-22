0f33f0ad20542069804bffafd0bb5eb516d174cf
mlange-42 <martin_lange_@gmx.net>
2025-11-23 00:06:41 +0100

    move plots to systems

--- demos/grazers/resources.jl
+++ demos/grazers/resources.jl
@@ -1,3 +1,14 @@
 struct GrassGrid
     grass::Observable{Array{Float64,2}}
 end
+
+struct WorldSize
+    width::Int
+    height::Int
+    scale::Int
+end
+
+struct Window
+    scene::GLMakie.Scene
+    screen::GLMakie.Screen
+end
---
+++ demos/grazers/sys/draw_grass.jl
@@ -0,0 +1,14 @@
+
+mutable struct DrawGrass <: System
+end
+
+function initialize!(s::DrawGrass, world::World)
+    size = get_resource(world, WorldSize)
+    win = get_resource(world, Window)
+    grass = get_resource(world, GrassGrid)
+
+    black_to_green = [RGB(0, 0, 0), RGB(0, 1, 0)]
+    hm = heatmap!(win.scene, 1:size.width, 1:size.height, grass.grass, colormap=black_to_green, colorrange=(0, 1))
+
+    Makie.transform!(hm, scale=Vec3f(size.scale, size.scale, 1))
+end
