8336343837c91a3b48a7ee4e9a0f699d3d69a42f
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-23 20:03:06 +0100

    Update resources.jl

--- demos/sir/resources.jl
+++ demos/sir/resources.jl
@@ -4,6 +4,14 @@ struct Buffer
     s_to_i::Vector{Entity}
     rands::Vector{Float64}
     ents::Vector{Entity}
+    function Buffer(i_to_r, s_to_i, rands, ents)
+        # hint to max capacity for more fluid simulations
+        sizehint!(i_to_r, 10^7)
+        sizehint!(s_to_i, 10^7)
+        sizehint!(rands, 10^7)
+        sizehint!(ents, 10^7)
+        return new(i_to_r, s_to_i, rands, ents)
+    end
 end
 
 mutable struct Params
