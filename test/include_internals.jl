7b3a6c36f20f9ab4dc19c130de096c4416d08f92
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-24 01:25:37 +0100

    Update include_internals.jl

--- test/include_internals.jl
+++ test/include_internals.jl
@@ -4,7 +4,6 @@ using Ark: _find_or_create_archetype!, _create_entity!, _move_entity!, _get_stor
 using Ark: _new_entity, _EntityIndex, _ComponentStorage
 using Ark: _new_entities_column
 using Ark: _Archetype, _add_entity!
-using Ark: _ComponentRegistry, _get_id!, _register_component!
 using Ark: _Mask, _Not
 using Ark: _get_bit, _contains_all, _contains_any, _and, _or, _clear_bits, _active_bit_indices
 using Ark: _is_zero, _is_not_zero
