cbc12f6948e542c339dda0d5ebe236d8bcf4f868
mlange-42 <martin_lange_@gmx.net>
2025-11-25 09:09:03 +0100

    implement gras growth

--- demos/grazers/sys/draw_grazers.jl
+++ demos/grazers/sys/draw_grazers.jl
@@ -11,7 +11,7 @@ function initialize!(s::DrawGrazers, world::World)
     positions = Observable([Point2f(rand() * size.width, rand() * size.height) for _ in 1:100])
     rotations = Observable([rand() * 2π for _ in 1:100])
 
-    sc = meshscatter!(win.scene, positions; rotation=rotations, marker=boid_shape, color=:white, markersize=0.1)
+    sc = meshscatter!(win.scene, positions; rotation=rotations, marker=boid_shape, color=:blue, markersize=0.1)
 
     Makie.transform!(sc, scale=Vec3f(size.scale, size.scale, 1))
 end
---
+++ demos/grazers/sys/grow_grass.jl
@@ -0,0 +1,26 @@
+
+mutable struct GrowGrass <: System
+    growth_rate::Float64
+end
+
+GrowGrass(;
+    growth_rate::Float64=0.01,
+) = GrowGrass(growth_rate)
+
+function initialize!(s::GrowGrass, world::World)
+    size = get_resource(world, WorldSize)
+    grass = GrassGrid(Observable([rand() * 0.25 + 0.25 for x in 1:size.width, y in 1:size.height]))
+    add_resource!(world, grass)
+end
+
+function update!(s::GrowGrass, world::World)
+    grass = get_resource(world, GrassGrid)
+
+    vals = grass.grass[]
+    r = s.growth_rate
+    for j in axes(vals, 2), i in axes(vals, 1)
+        v = vals[i, j]
+        v += r * v * (1.0 - v)
+        vals[i, j] = clamp(v, 0, 1)
+    end
+end
