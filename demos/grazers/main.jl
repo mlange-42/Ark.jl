6581233e9a800e63e1957ded735a470b69a0604a
mlange-42 <martin_lange_@gmx.net>
2025-11-25 09:09:03 +0100

    move plots to systems

--- demos/grazers/main.jl
+++ demos/grazers/main.jl
@@ -3,42 +3,47 @@ using Colors
 using GLMakie
 using GeometryBasics
 
+include("../_common/scheduler.jl")
+include("../_common/resources.jl")
+include("../_common/terminate.jl")
 include("resources.jl")
+include("sys/draw_grass.jl")
+include("sys/draw_grazers.jl")
+
+const IS_CI = "CI" in keys(ENV)
 
 function main()
-    width = 80
-    height = 60
+    size = WorldSize(80, 60, 10)
     GLMakie.activate!(
         framerate=60.0,
         vsync=true,
         renderloop=GLMakie.renderloop,
         render_on_demand=true,
     )
-    scene = Scene(camera=campixel!, size=(width * 10, height * 10), backgroundcolor=:black)
+    scene = Scene(camera=campixel!, size=(size.width * size.scale, size.height * size.scale), backgroundcolor=:black)
     screen = display(scene)
 
     world = World()
-    grass = GrassGrid(Observable([sin(x / 10) * cos(y / 10) for x in 1:width, y in 1:height]))
+    add_resource!(world, size)
+    grass = GrassGrid(Observable([sin(x / 10) * cos(y / 10) for x in 1:size.width, y in 1:size.height]))
     add_resource!(world, grass)
+    add_resource!(world, Window(scene, screen))
 
-    black_to_green = [RGB(0, 0, 0), RGB(0, 1, 0)]
-    hm = heatmap!(scene, 1:width, 1:height, grass.grass, colormap=black_to_green, colorrange=(0, 1))
+    scheduler = Scheduler(
+        world,
+        (
+            DrawGrass(),
+            DrawGrazers(),
+            TerminationSystem(IS_CI ? 240 : -1), # Short run in CI tests
+        ),
+    )
 
-    boid_shape = Polygon(Point2f[(0, 3), (4, -5), (0, -3), (-4, -5)])
-
-    positions = Observable([Point2f(rand() * 80, rand() * 60) for _ in 1:100])
-    rotations = Observable([rand() * 2π for _ in 1:100])
-
-    sc = meshscatter!(scene, positions; rotation=rotations, marker=boid_shape, color=:white, markersize=0.1)
-
-    Makie.transform!(hm, scale=Vec3f(10, 10, 1))
-    Makie.transform!(sc, scale=Vec3f(10, 10, 1))
+    initialize!(scheduler)
 
     on(screen.render_tick) do _
-        for i in eachindex(rotations[])
-            rotations[][i] += 0.01
+        if !update!(scheduler)
+            GLMakie.closeall()
         end
-        notify(rotations)
     end
 
     GLMakie.start_renderloop!(screen)
---
+++ demos/grazers/sys/draw_grazers.jl
@@ -0,0 +1,17 @@
+
+mutable struct DrawGrazers <: System
+end
+
+function initialize!(s::DrawGrazers, world::World)
+    size = get_resource(world, WorldSize)
+    win = get_resource(world, Window)
+
+    boid_shape = Polygon(Point2f[(0, 3), (4, -5), (0, -3), (-4, -5)])
+
+    positions = Observable([Point2f(rand() * size.width, rand() * size.height) for _ in 1:100])
+    rotations = Observable([rand() * 2π for _ in 1:100])
+
+    sc = meshscatter!(win.scene, positions; rotation=rotations, marker=boid_shape, color=:white, markersize=0.1)
+
+    Makie.transform!(sc, scale=Vec3f(size.scale, size.scale, 1))
+end
