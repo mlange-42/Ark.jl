a3f735ae9e53ce717d0a3146e82df6324ccfcea0
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-26 02:44:06 +0100

    Use indices for neighbors instead of nodes

--- src/vec_map.jl
+++ src/vec_map.jl
@@ -1,13 +1,13 @@
-
+# src/vec_map.jl
 const _vec_map_chunk_size = 16
 
-struct _VecMap{T,M}
-    data::Vector{T}
+struct _VecMap{M}
+    data::Vector{Int}
     used::_MutableMask{M}
 end
 
-function _VecMap{T,M}() where {T,M}
-    _VecMap{T,M}(Vector{T}(undef, 5), _MutableMask{M}())
+function _VecMap{M}() where {M}
+    _VecMap{M}(zeros(Int, 5), _MutableMask{M}())
 end
 
 function _get_map(m::_VecMap, index::Int)
@@ -17,10 +17,12 @@ function _get_map(m::_VecMap, index::Int)
     @inbounds return m.data[index]
 end
 
-function _set_map!(m::_VecMap{T}, index::Int, value::T) where T
-    if length(m.data) < index
-        size = (index + _vec_map_chunk_size) & -_vec_map_chunk_size
-        resize!(m.data, size)
+function _set_map!(m::_VecMap, index::Int, value::Int)
+    curr_len = length(m.data)
+    if curr_len < index
+        new_len = (index + _vec_map_chunk_size) & -_vec_map_chunk_size
+        resize!(m.data, new_len)
+        @inbounds m.data[curr_len + 1:new_len] .= 0
     end
     _set_bit!(m.used, index)
     @inbounds m.data[index] = value
