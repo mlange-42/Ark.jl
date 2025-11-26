8d724c8e1ab573015e5887ad99bba6250bd65cc1
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-11-26 03:05:24 +0100

    Update vec_map.jl

--- src/vec_map.jl
+++ src/vec_map.jl
@@ -7,7 +7,7 @@ struct _VecMap{M}
 end
 
 function _VecMap{M}() where {M}
-    _VecMap{M}(zeros(Int, 5), _MutableMask{M}())
+    _VecMap{M}(Vector{Int}(undef, 5), _MutableMask{M}())
 end
 
 function _get_map(m::_VecMap, index::Int)
@@ -17,12 +17,10 @@ function _get_map(m::_VecMap, index::Int)
     @inbounds return m.data[index]
 end
 
-@inline function _set_map!(m::_VecMap, index::Int, value::Int)
-    curr_len = length(m.data)
-    if curr_len < index
+function _set_map!(m::_VecMap, index::Int, value::Int)
+    if length(m.data) < index
         new_len = (index + _vec_map_chunk_size) & -_vec_map_chunk_size
         resize!(m.data, new_len)
-        @inbounds m.data[curr_len + 1:new_len] .= 0
     end
     _set_bit!(m.used, index)
     @inbounds m.data[index] = value
