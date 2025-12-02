2ece09576bacf1a83590b57facd15a11d14fe9b4
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-02 01:43:53 +0100

    Add benchmarking for get map functionality

---
+++ benchmark/internal/bench_get_map.jl
@@ -0,0 +1,24 @@
+
+using Random
+using Ark: _Mask_Map
+
+rng = Xoshiro(42)
+
+function setup_get_map(n)
+    map = _Mask_Map{1, Int}()
+    rands = rand(rng, n, 1:n)
+    return map, rands
+end
+
+function benchmark_get_map(args)
+    map, rands = args
+    s = 0
+    for r in rands
+        s += get!(() -> 1, map, r)
+    end
+    return s
+end
+
+for n in (100, 10000)
+    SUITE["benchmark_get_map n=$n"] = @be setup_get_map(n) benchmark_get_map(_) seconds = SECONDS
+end
