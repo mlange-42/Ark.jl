dd3bcb0dcecefabee046a43d535d9ccb9da622b9
mlange-42 <martin_lange_@gmx.net>
2025-12-03 16:35:04 +0100

    benchmark thread-safe world lock

--- src/lock.jl
+++ src/lock.jl
@@ -1,15 +1,24 @@
 mutable struct _Lock
     const pool::_BitPool
     lock_bits::UInt64
+    thread_lock::ReentrantLock
 end
 
 function _Lock()
-    _Lock(_BitPool(), 0)
+    _Lock(_BitPool(), 0, ReentrantLock())
 end
 
 function _lock(lock::_Lock)::Int
     l = _get_bit(lock.pool)
     lock.lock_bits |= UInt64(1) << ((l - 1) % UInt64)
+    return l
+end
+
+function _lock_safe(lo::_Lock)::Int
+    l = 0
+    lock(lo.thread_lock)
+    l = _lock(lo)
+    unlock(lo.thread_lock)
     return l
 end
 
@@ -24,6 +33,22 @@ function _unlock(lock::_Lock, b::Int)
     end
     lock.lock_bits &= ~(UInt64(1) << ((b - 1) % UInt64))
     _recycle(lock.pool, b)
+end
+
+function _unlock_safe(lo::_Lock, b::Int)
+    lock(lo.thread_lock)
+    if ((lo.lock_bits >> (b - 1)) & 0x01) == 0
+        unlock(lo.thread_lock)
+        throw(
+            InvalidStateException(
+                "unbalanced unlock. Did you close a query that was already iterated?",
+                :unbalanced_lock,
+            ),
+        )
+    end
+    lo.lock_bits &= ~(UInt64(1) << (b - 1))
+    _recycle(lo.pool, b)
+    unlock(lo.thread_lock)
 end
 
 function _is_locked(lock::_Lock)::Bool
