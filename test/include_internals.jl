e0b0c7cf3783eeaed641b5058aa5a49c22fc4b5a
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-12-04 15:32:44 +0100

    Optimize `get_components` (#371)

--- test/include_internals.jl
+++ test/include_internals.jl
@@ -1,7 +1,7 @@
 
 using Ark
 using Ark: _find_or_create_archetype!, _find_or_create_table!
-using Ark: _create_entity!, _move_entity!, _get_storage, _component_id, _get_relations
+using Ark: _create_entity!, _move_entity!, _get_storage, _component_id, _get_relations, _get_relations_storage
 using Ark: _new_entity, _EntityIndex, _ComponentStorage
 using Ark: _new_entities_column
 using Ark: _Archetype, _add_entity!, _has_relations
