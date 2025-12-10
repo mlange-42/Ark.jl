bd4c3df2a7b88029e1a9e2aa3e56978f299b801f
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-10 18:16:50 +0100

    Update ArkMooncakeInterop.jl

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -1,13 +1,18 @@
 
 module ArkMooncakeInterop
 
-using Ark, Mooncake
+using Ark, Mooncake, Mooncake.Random
 
 Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
 Mooncake.tangent_type(::Type{<:Ark._EventManager}) = Mooncake.NoTangent
 
-Mooncake.@mooncake_overlay function Base.sizehint!(A::AbstractVector, n::Integer)
+Mooncake.@mooncake_overlay Base.sizehint!(A::AbstractVector, n::Integer) = A
+Mooncake.@mooncake_overlay Random.rand!(A::AbstractArray) = rand!(Random.default_rng(), A)
+Mooncake.@mooncake_overlay function Random.rand!(rng::Random.AbstractRNG, A::AbstractArray)
+    for i in eachindex(A)
+        A[i] = rand(rng)
+    end
     return A
 end
 
