4d95cbc3b054438392fe8e06f2914cefe50e958b
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-10 23:31:20 +0100

    Add files via upload

---
+++ test/ext/test_mooncake_interop.jl
@@ -0,0 +1,55 @@
+
+using DifferentiationInterface
+using Mooncake
+
+struct Position
+    x::Float64
+    y::Float64
+end
+
+struct Velocity
+    dx::Float64
+    dy::Float64
+end
+
+@testset "Compute gradients through Mooncake.jl" begin
+	function run_world(args)
+		alpha, beta = args
+		world = World(Position, Velocity)
+
+		entities = Entity[]
+		for i in 1:1000
+	    	entity = new_entity!(world, (Position(i, i * 2),))
+	    	push!(entities, entity)
+		end
+
+		for e in entities
+			add_components!(world, e, (Velocity(1, 1),))
+		end
+
+		for i in 1:10
+		    for (entities, positions, velocities) in Query(world, (Position, Velocity))
+		        @inbounds for i in eachindex(entities)
+		            pos = positions[i]
+		            vel = velocities[i]
+		            positions[i] = Position(alpha * (pos.x + vel.dx), beta * (pos.y + vel.dy))
+		        end
+		    end
+		end
+
+		all_positions = Position[]
+		for (entities, positions) in Query(world, (Position,))
+			append!(all_positions, positions)
+		end
+
+		return sum(pos.x + pos.y for pos in all_positions)
+	end
+
+	backend = AutoMooncake()
+
+	g = gradient(run_world, backend, (0.1, 0.5))
+	d_alpha, d_beta = g
+
+	@test run_world((0.1 + 1/100000, 0.5)) - run_world((0.1, 0.5)) - d_alpha / 100000 < 10e-4
+	@test run_world((0.1, 0.5 + 1/100000)) - run_world((0.1, 0.5)) - d_beta / 100000 < 10e-4
+end
