aa1d16a955fef8d7fc13886a992845068f7eaf3a
tortar <adrianomeligrana1999@gmail.com>
2025-12-11 03:17:21 +0100

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,49 +3,42 @@ ENV["ARK_RUNNING_TESTS"] = true
 
 if "--interop" in ARGS
     include("ext/runtests.jl")
-    begin 
-        @goto stop
-    end
-end
-
-using Test
-using JET
-
-include("include_internals.jl")
-
-if "--large-world" in ARGS
-    include("setup_large.jl")
 else
-    include("setup_default.jl")
-end
+    using Test
+    using JET
 
-# TODO: re-enable when fixed on the Julia side.
-const RUN_JET = "CI" in keys(ENV) && VERSION >= v"1.12.0" && isempty(VERSION.prerelease)
+    include("include_internals.jl")
 
-include("TestTypes.jl")
+    if "--large-world" in ARGS
+        include("setup_large.jl")
+    else
+        include("setup_default.jl")
+    end
 
-include("test_util.jl")
-include("test_world.jl")
-include("test_cache.jl")
-include("test_filter.jl")
-include("test_query.jl")
-include("test_event.jl")
-include("test_relations.jl")
-include("test_archetype.jl")
-include("test_structarray.jl")
-include("test_readme.jl")
-include("test_entity.jl")
-include("test_pool.jl")
-include("test_lock.jl")
-include("test_mask.jl")
-include("test_registry.jl")
-include("test_vec_map.jl")
-include("test_linear_map.jl")
-include("test_graph.jl")
-include("test_quality.jl")
+    # TODO: re-enable when fixed on the Julia side.
+    const RUN_JET = "CI" in keys(ENV) && VERSION >= v"1.12.0" && isempty(VERSION.prerelease)
 
-begin
-    @label stop
+    include("TestTypes.jl")
+
+    include("test_util.jl")
+    include("test_world.jl")
+    include("test_cache.jl")
+    include("test_filter.jl")
+    include("test_query.jl")
+    include("test_event.jl")
+    include("test_relations.jl")
+    include("test_archetype.jl")
+    include("test_structarray.jl")
+    include("test_readme.jl")
+    include("test_entity.jl")
+    include("test_pool.jl")
+    include("test_lock.jl")
+    include("test_mask.jl")
+    include("test_registry.jl")
+    include("test_vec_map.jl")
+    include("test_linear_map.jl")
+    include("test_graph.jl")
+    include("test_quality.jl")
 end
 
 ENV["ARK_RUNNING_TESTS"] = false
