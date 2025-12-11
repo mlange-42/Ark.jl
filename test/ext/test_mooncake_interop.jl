fad096b11e915cee6213a8082b907c2cbf3eb3d4
tortar <adrianomeligrana1999@gmail.com>
2025-12-11 02:36:17 +0100

    fix

--- test/ext/test_mooncake_interop.jl
+++ test/ext/test_mooncake_interop.jl
@@ -13,43 +13,43 @@ struct Velocity
 end
 
 @testset "Compute gradients through Mooncake.jl" begin
-	function run_world(args)
-		alpha, beta = args
-		world = World(Position, Velocity)
+    function run_world(args)
+        alpha, beta = args
+        world = World(Position, Velocity)
 
-		entities = Entity[]
-		for i in 1:1000
-	    	entity = new_entity!(world, (Position(i, i * 2),))
-	    	push!(entities, entity)
-		end
+        entities = Entity[]
+        for i in 1:1000
+            entity = new_entity!(world, (Position(i, i * 2),))
+            push!(entities, entity)
+        end
 
-		for e in entities
-			add_components!(world, e, (Velocity(1, 1),))
-		end
+        for e in entities
+            add_components!(world, e, (Velocity(1, 1),))
+        end
 
-		for i in 1:10
-		    for (entities, positions, velocities) in Query(world, (Position, Velocity))
-		        @inbounds for i in eachindex(entities)
-		            pos = positions[i]
-		            vel = velocities[i]
-		            positions[i] = Position(alpha * (pos.x + vel.dx), beta * (pos.y + vel.dy))
-		        end
-		    end
-		end
+        for i in 1:10
+            for (entities, positions, velocities) in Query(world, (Position, Velocity))
+                @inbounds for i in eachindex(entities)
+                    pos = positions[i]
+                    vel = velocities[i]
+                    positions[i] = Position(alpha * (pos.x + vel.dx), beta * (pos.y + vel.dy))
+                end
+            end
+        end
 
-		all_positions = Position[]
-		for (entities, positions) in Query(world, (Position,))
-			append!(all_positions, positions)
-		end
+        all_positions = Position[]
+        for (entities, positions) in Query(world, (Position,))
+            append!(all_positions, positions)
+        end
 
-		return sum(pos.x + pos.y for pos in all_positions)
-	end
+        return sum(pos.x + pos.y for pos in all_positions)
+    end
 
-	backend = AutoMooncake()
+    backend = AutoMooncake()
 
-	g = gradient(run_world, backend, (0.1, 0.5))
-	d_alpha, d_beta = g
+    g = gradient(run_world, backend, (0.1, 0.5))
+    d_alpha, d_beta = g
 
-	@test run_world((0.1 + 10e-5, 0.5)) - run_world((0.1, 0.5)) - d_alpha * 10e-5 < 10e-3
-	@test run_world((0.1, 0.5 + 10e-5)) - run_world((0.1, 0.5)) - d_beta * 10e-5 < 10e-3
+    @test 0.99 < (run_world((0.1 + 10e-5, 0.5)) - run_world((0.1, 0.5))) / (d_alpha * 10e-5) < 1.01
+    @test 0.99 < (run_world((0.1, 0.5 + 10e-5)) - run_world((0.1, 0.5))) / (d_beta * 10e-5) < 1.01
 end
