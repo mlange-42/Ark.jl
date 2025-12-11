ad466aa194c037a5836e71e46227d3c91276f745
tortar <adrianomeligrana1999@gmail.com>
2025-12-11 03:09:05 +0100

    fix

--- run_tests.jl
+++ run_tests.jl
@@ -1,3 +1,9 @@
 using Pkg
-Pkg.activate(ARGS[1])
-Pkg.test("Ark"; test_args=[ARGS[2],])
+
+if length(ARGS) == 2
+	Pkg.activate(ARGS[1])
+	Pkg.test("Ark"; test_args=[ARGS[2],])
+else
+	Pkg.activate(".")
+	Pkg.test("Ark"; test_args=ARGS)
+end
