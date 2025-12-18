ea29a5640fb23ab137776ccfd712cac5de7b6891
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-18 23:01:46 +0100

    Update test_pool.jl

--- test/test_pool.jl
+++ test/test_pool.jl
@@ -9,6 +9,48 @@
     @test pool.next == 0
 end
 
+@testset "_EntityPool logic" begin
+    # Setup
+    pool = _EntityPool(UInt32(10))  # creates 2 reserved entities
+
+    @test length(pool.entities) == 1
+    @test pool.next == 0
+
+    @test _is_alive(pool, zero_entity) == false
+
+    # Test _get_entity when no available entities
+    e1 = _get_entity(pool)
+    @test isa(e1, Entity)
+    @test e1._id == 2
+    @test e1._gen == 0
+    @test length(pool.entities) == 2
+
+    # Test _get_entity again
+    e2 = _get_entity(pool)
+    @test e2._id == 3
+    @test e2._gen == 0
+    @test length(pool.entities) == 3
+
+    # Test _recycle with non-reserved entity
+    _recycle(pool, e1)
+    @test pool.next == e1._id
+    @test pool.entities[e1._id]._gen == e1._gen + 1
+
+    # Test _get_entity now uses recycled entity
+    e3 = _get_entity(pool)
+    @test e3._id == e1._id
+    @test e3._gen == e1._gen + 1
+    @test pool.next == 0
+
+    # Test _alive
+    @test _is_alive(pool, e2) == true
+    @test _is_alive(pool, e3) == true
+    @test _is_alive(pool, e1) == false  # old generation
+
+    # Test _recycle throws on reserved entity
+    @test_throws "ArgumentError: can't recycle the reserved zero entity" _recycle(pool, zero_entity)
+end
+
 @testset "_BitPool basic functionality" begin
     pool = _BitPool()
 
