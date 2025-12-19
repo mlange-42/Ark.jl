ef2d004c9e3cd56cfe605b24637b32dc9b3fd6c5
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-19 09:53:13 +0100

    Update test_world.jl

--- test/test_world.jl
+++ test/test_world.jl
@@ -997,6 +997,34 @@ end
         remove_components!(world, zero_entity, (Position, Velocity)))
     @test_throws("ArgumentError: can't check components of a dead entity",
         has_components(world, zero_entity, (Position, Velocity)))
+
+    world = World(Position, Velocity)
+
+    entity1 = new_entity!(world, (Position(1.0, 2.0), Velocity(3.0, 4.0)))
+    entity2 = new_entity!(world, (Position(5.0, 6.0), Velocity(7.0, 8.0)))
+
+    @test get_components(world, entity1, (Position, Velocity,)) == (Position(1.0, 2.0), Velocity(3.0, 4.0))
+    @test get_components(world, entity2, (Position, Velocity,)) == (Position(5.0, 6.0), Velocity(7.0, 8.0))
+
+    remove_components!(world, entity1, (Position,))
+
+    @test get_components(world, entity1, (Velocity,)) == (Velocity(3.0, 4.0),)
+    @test get_components(world, entity2, (Position, Velocity,)) == (Position(5.0, 6.0), Velocity(7.0, 8.0))
+
+    remove_components!(world, entity1, (Velocity,))
+
+    @test get_components(world, entity1, ()) == ()
+    @test get_components(world, entity2, (Position, Velocity,)) == (Position(5.0, 6.0), Velocity(7.0, 8.0))
+
+    remove_components!(world, entity2, (Position,))
+
+    @test get_components(world, entity1, ()) == ()
+    @test get_components(world, entity2, (Velocity,)) == (Velocity(7.0, 8.0),)
+
+    remove_components!(world, entity2, (Velocity,))
+
+    @test get_components(world, entity1, ()) == ()
+    @test get_components(world, entity2, ()) == ()
 end
 
 @testset "World add/remove components with relations" begin
