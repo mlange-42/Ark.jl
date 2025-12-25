5c974db8161558345cb18184001846dbc3bd5a43
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-25 20:15:53 +0100

    Fix indentation in benchmark for add/remove components

--- benchmark/benches/bench_world_add_remove_8_large.jl
+++ benchmark/benches/bench_world_add_remove_8_large.jl
@@ -27,6 +27,8 @@ function setup_world_add_remove_8_large(n_entities::Int)
             (CompN{1}(0, 0), CompN{2}(0, 0), CompN{3}(0, 0), CompN{4}(0, 0),
                 CompN{5}(0, 0), CompN{6}(0, 0), CompN{7}(0, 0), CompN{8}(0, 0)),
         )
+    end
+    for e in entities
         remove_components!(world, e,
             (CompN{1}, CompN{2}, CompN{3}, CompN{4},
                 CompN{5}, CompN{6}, CompN{7}, CompN{8}),
