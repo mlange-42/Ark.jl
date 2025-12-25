e80841b7d4d078cc7842a3a3c7e7d7619b2ed8a7
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-25 20:01:26 +0100

    Add missing end for first loop in benchmark function

--- benchmark/benches/bench_world_add_remove_1_large.jl
+++ benchmark/benches/bench_world_add_remove_1_large.jl
@@ -35,6 +35,8 @@ function benchmark_world_add_remove_1_large(args, n)
     entities, world = args
     for e in entities
         add_components!(world, e, (Velocity(0, 0),))
+    end
+    for e in entities
         remove_components!(world, e, (Velocity,))
     end
 end
