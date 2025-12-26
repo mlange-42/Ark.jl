9d2b1b4c4259d6b0f21a952518060129ce5bd9df
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-26 16:36:17 +0100

    Update bench_world_get_1_soa.jl

--- benchmark/benches/bench_world_get_1_soa.jl
+++ benchmark/benches/bench_world_get_1_soa.jl
@@ -25,7 +25,7 @@ function benchmark_world_get_1_soa(args, n)
     entities, world = args
     sum = 0.0
     for e in entities
-        pos, = get_components(world, e, (Position,))
+        pos, = @inbounds get_components(world, e, (Position,))
         sum += pos.x
     end
 end
